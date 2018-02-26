6bb6f6fcf20631b1e973d28b8376fbcbf665cf9f
Mohamed Elmahalwy <mohamedelmahlawy53@gmail.com>
2018-02-26 16:46:22 +0200

    c

---
+++ app/src/main/java/com/elmahalwy/thirdproject/Activties/StepsActivity.java
@@ -0,0 +1,15 @@
+package com.elmahalwy.thirdproject.Activties;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.elmahalwy.thirdproject.R;
+
+public class StepsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steps);
+    }
+}
---
+++ app/src/main/java/com/elmahalwy/thirdproject/Adapters/DetailsAdapter.java
@@ -0,0 +1,67 @@
+package com.elmahalwy.thirdproject.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.elmahalwy.thirdproject.Models.MainModel;
+import com.elmahalwy.thirdproject.Models.StepsModel;
+import com.elmahalwy.thirdproject.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by cz on 26/02/2018.
+ */
+
+public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.ViewHolder> {
+    List<StepsModel> steps_list = new ArrayList<>();
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public DetailsAdapter(List<StepsModel> steps_list, Context context) {
+        this.steps_list = steps_list;
+        this.context = context;
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public DetailsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = layoutInflater.from(parent.getContext()).inflate(R.layout.details_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(DetailsAdapter.ViewHolder holder, int position) {
+        holder.tv_step_name.setText(steps_list.get(position).getShortDescription());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return steps_list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_step_name)
+        TextView tv_step_name;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
