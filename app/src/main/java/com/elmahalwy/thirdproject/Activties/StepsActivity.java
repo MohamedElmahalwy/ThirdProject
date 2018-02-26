5dc82f86e6c289b85eb6cc03693e066942246277
Mohamed Elmahalwy <mohamedelmahlawy53@gmail.com>
2018-02-26 18:51:38 +0200

    f

--- app/src/main/java/com/elmahalwy/thirdproject/Activties/StepsActivity.java
+++ app/src/main/java/com/elmahalwy/thirdproject/Activties/StepsActivity.java
@@ -1,15 +1,29 @@
 package com.elmahalwy.thirdproject.Activties;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.elmahalwy.thirdproject.R;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class StepsActivity extends AppCompatActivity {
     @BindView(R.id.tv_no_video)
@@ -18,11 +32,13 @@ public class StepsActivity extends AppCompatActivity {
     TextView tv_step_description;
     @BindView(R.id.video_steps)
     SimpleExoPlayerView video_steps;
+    SimpleExoPlayer Player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steps);
+        ButterKnife.bind(this);
         InitEventDriven();
     }
 
@@ -30,12 +46,43 @@ public class StepsActivity extends AppCompatActivity {
         tv_step_description.setText(getIntent().getStringExtra("description"));
         if (getIntent().getStringExtra("video_url").isEmpty()) {
             tv_no_video.setVisibility(View.VISIBLE);
-            video_steps.setVisibility(View.GONE);
+            video_steps.setVisibility(View.INVISIBLE);
             tv_no_video.setText("No videos");
 
         } else {
             tv_no_video.setVisibility(View.GONE);
-
+            video_steps.setVisibility(View.VISIBLE);
+            PlayVideo(Uri.parse(getIntent().getStringExtra("video_url")));
         }
+    }
+
+    void PlayVideo(Uri uri) {
+        if (Player == null) {
+            TrackSelector selector = new DefaultTrackSelector();
+            LoadControl control = new DefaultLoadControl();
+            Player = ExoPlayerFactory.newSimpleInstance(this, selector, control);
+            video_steps.setPlayer(Player);
+            String user = Util.getUserAgent(this, "ThirdProject");
+            MediaSource mediaSource = new ExtractorMediaSource(uri, new DefaultDataSourceFactory(this, user), new DefaultExtractorsFactory()
+                    , null, null);
+
+            Player.prepare(mediaSource);
+            Player.setPlayWhenReady(true);
+        }
+    }
+
+    void stopPlayer() {
+        if (Player != null) {
+
+            Player.stop();
+            Player.release();
+            Player = null;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopPlayer();
     }
 }
